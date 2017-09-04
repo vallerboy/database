c9be93259b85a3bf2f280d073125449969542f29
Oskar Polak <oskarbank@gmail.com>
2017-09-04 16:05:58 +0200

    asd

--- src/main/java/pl/oskarpolak/database/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/database/controllers/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import pl.oskarpolak.database.models.EmailModel;
 import pl.oskarpolak.database.models.forms.EmailForm;
 import pl.oskarpolak.database.models.repositories.EmailRepostiory;
+import pl.oskarpolak.database.models.services.EmailService;
 
 /**
  * Created by Lenovo on 04.09.2017.
@@ -17,6 +18,9 @@ public class MainController {
     @Autowired
     EmailRepostiory emailRepostiory;
 
+    @Autowired
+    EmailService emailService;
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("formEmail", new EmailForm());
@@ -25,7 +29,9 @@ public class MainController {
 
     @PostMapping("/")
     public String index(@ModelAttribute("formEmail") EmailForm form, Model model){
-        emailRepostiory.save(new EmailModel(form));
+        EmailModel emailModel = new EmailModel(form);
+        emailRepostiory.save(emailModel);
+        emailService.sendEmail(emailModel);
 
         model.addAttribute("message", "Wiadomość został wysłana. ");
 
